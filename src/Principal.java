eba6bb0ae011fe3febebd595bfd4f8e9d363fbe3
Hector Vazquez <hectorr11330@gmail.com>
2024-05-11 09:29:24 -0600

    Actualizacion

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,33 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Principal {
-    public static void main(String[] args) {
-        System.out.println("Hola Alura");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        String direccion = "https://v6.exchangerate-api.com/v6/255be9bc48338f627cc2d55c/latest/USD";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+
     }
 }
